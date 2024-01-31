66972efa03b9d796aa5bf5aaeab00c1bce599db7
huh445 <127354663+huh445@users.noreply.github.com>
2024-01-31 18:28:19 +1100

    Added spacing and comments
    Updated readability and added some LED compatibility to show when the task is currently happening or stopped.
    
    Also, closed powershell when the task is completed.

--- Background-Changer/Background-Changer.ino
+++ Background-Changer/Background-Changer.ino
@@ -4,29 +4,40 @@
 #include "DigiKeyboard.h"
 #define link "https://img.freepik.com/free-photo/painting-mountain-lake-with-mountain-background_188544-9126.jpg"
 void setup() {
-  //empty
+  pinMode(1, OUTPUT); // LED Setup
 }
 void loop() {
   DigiKeyboard.sendKeyStroke(0);
-  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
+  DigiKeyboard.delay(1000);
+
+  digitalWrite(1, HIGH); // Turn on LED to signify task started
+
+  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT); // Go to desktop
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
+
+  DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT); // Open Run
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("powershell");
+
+  DigiKeyboard.println("powershell"); // Open Powershell
   DigiKeyboard.delay(2000);
+
   DigiKeyboard.println("$client = new-object System.Net.WebClient");
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("Remove-Item $env:USERPROFILE\\devon.jpg");
+  DigiKeyboard.println("Remove-Item $env:USERPROFILE\\devon.jpg"); // Remove the image if it already exists
   DigiKeyboard.delay(500);
-  DigiKeyboard.println("$client.DownloadFile(\""link"\" , \"devon.jpg\")");
+  DigiKeyboard.println("$client.DownloadFile(\""link"\" , \"devon.jpg\")"); // Download the image
   DigiKeyboard.delay(5000);
-  DigiKeyboard.print("set-itemproperty -path ""HKCU:\\Control Panel\\Desktop\\"" -name Wallpaper -value $env:USERPROFILE\\devon.jpg");
+
+  DigiKeyboard.print("set-itemproperty -path ""HKCU:\\Control Panel\\Desktop\\"" -name Wallpaper -value $env:USERPROFILE\\devon.jpg"); // Set the wallpaper
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_ENTER);
+  DigiKeyboard.sendKeyStroke(KEY_ENTER); // Additional delay for better compatibility
   DigiKeyboard.delay(1000);
-  DigiKeyboard.println("rundll32.exe user32.dll, UpdatePerUserSystemParameters");
+
+  DigiKeyboard.println("rundll32.exe user32.dll, UpdatePerUserSystemParameters"); // Update the wallpaper
   DigiKeyboard.delay(500);
-  DigiKeyboard.sendKeyStroke(KEY_D, MOD_GUI_LEFT);
-  DigiKeyboard.delay(500);
-  for(;;){ /*empty*/ }
+  
+  DigiKeyboard.println("exit"); // Exit Powershell
+
+  digitalWrite(1, LOW); // Turn off LED to signify task completed
+  for(;;){} // We don't like looping.
 }
