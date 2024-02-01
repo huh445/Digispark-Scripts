afe4ac399fd188684be52dfe8ab21527e00f7a21
huh445 <127354663+huh445@users.noreply.github.com>
2024-02-01 14:56:52 +1100

    IT WORKS

--- Background-Changer/Background-Changer.ino
+++ Background-Changer/Background-Changer.ino
@@ -3,6 +3,7 @@
 // Change the link to the image you want to use in jpg form.
 #include "DigiKeyboard.h"
 #define link "https://i.ibb.co/jg1v95Y/unnamed.png"
+#define KEY_DOWN 0x51
 void setup() {
   pinMode(1, OUTPUT); // LED Setup
 }
@@ -13,18 +14,25 @@ void loop() {
   DigiKeyboard.sendKeyStroke(KEY_R, MOD_GUI_LEFT);
   DigiKeyboard.delay(500);
 
+ DigiKeyboard.println("msconfig -5"); // Access CMD
+    DigiKeyboard.delay(1000);
+    for(int i = 0; i < 14; i++) {
+        DigiKeyboard.sendKeyStroke(KEY_DOWN);
+    }
+  DigiKeyboard.sendKeyStroke(KEY_L, MOD_ALT_LEFT);
+  DigiKeyboard.delay(1000);
+  
   DigiKeyboard.println("powershell");
-  DigiKeyboard.delay(2000);
   DigiKeyboard.println("$path = \"$env:USERPROFILE\\wallpaper.jpg\"");
   DigiKeyboard.println("$client = new-object System.Net.WebClient");
-  DigiKeyboard.delay(1000);
   DigiKeyboard.println("Remove-Item $path");
-  DigiKeyboard.delay(500);
-  DigiKeyboard.println("$client.DownloadFile(\""link"\" , \"wallpaper.jpg\")");
-  DigiKeyboard.delay(10000);
+  DigiKeyboard.println("$client.DownloadFile(\""link"\" , $path)");
+  DigiKeyboard.delay(5000);
 
-  DigiKeyboard.println("SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, $path, 0)");
-  DigiKeyboard.delay(500);
+  DigiKeyboard.println("Set-ItemProperty -Path \"HKCU:\\Control Panel\\Desktop\\\" -Name wallpaper -Value $path");
+  DigiKeyboard.delay(200);
+
+  DigiKeyboard.println("logoff");
   
   digitalWrite(1, LOW);
   for(;;){}
