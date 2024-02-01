4896a02919f2cba7e5b1a7f5e20d0db166cc18c8
huh445 <127354663+huh445@users.noreply.github.com>
2024-02-01 15:08:13 +1100

    works only on restart :(

--- Background-Changer/Background-Changer.ino
+++ Background-Changer/Background-Changer.ino
@@ -2,7 +2,7 @@
 // Tested working Windows 11. 
 // Change the link to the image you want to use in jpg form.
 #include "DigiKeyboard.h"
-#define link "https://i.ibb.co/jg1v95Y/unnamed.png"
+#define link "https://www.mannapro.com/sites/default/files/2023-09/15.png"
 #define KEY_DOWN 0x51
 void setup() {
   pinMode(1, OUTPUT); // LED Setup
@@ -26,11 +26,18 @@ void loop() {
   DigiKeyboard.println("$path = \"$env:USERPROFILE\\wallpaper.jpg\"");
   DigiKeyboard.println("$client = new-object System.Net.WebClient");
   DigiKeyboard.println("Remove-Item $path");
+  DigiKeyboard.delay(200);
   DigiKeyboard.println("$client.DownloadFile(\""link"\" , $path)");
-  DigiKeyboard.delay(5000);
+  DigiKeyboard.delay(10000);
 
   DigiKeyboard.println("Set-ItemProperty -Path \"HKCU:\\Control Panel\\Desktop\\\" -Name wallpaper -Value $path");
-  DigiKeyboard.delay(200);
+  DigiKeyboard.delay(500);
+  
+  DigiKeyboard.println("taskkill /f /im explorer.exe");
+  DigiKeyboard.delay(1000);
+
+  DigiKeyboard.println("start explorer.exe");
+  DigiKeyboard.delay(3000);
 
   DigiKeyboard.println("logoff");
   
