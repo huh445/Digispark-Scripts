78c769d40de4efee75f041afc7d4a17ba7d307be
huh445 <127354663+huh445@users.noreply.github.com>
2024-02-06 19:09:05 +1100

    im bad at grammar

--- picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
+++ picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("picker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2c45dbf597116a079c58d92fa1eb7873074e7af")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb2da86fad8d715c89552e641a591689aedd6de4")]
 [assembly: System.Reflection.AssemblyProductAttribute("picker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("picker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
