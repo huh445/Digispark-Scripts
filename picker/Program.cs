e2c45dbf597116a079c58d92fa1eb7873074e7af
huh445 <127354663+huh445@users.noreply.github.com>
2024-02-06 18:36:53 +1100

    QOL update

--- picker/Program.cs
+++ picker/Program.cs
@@ -1,24 +1,25 @@
 ﻿// C# program to upload Digispark sketches using Arduino CLI
 // Lightly Commented!
 using System;
-using System.Configuration.Assemblies;
 using System.Diagnostics;
 using System.IO;
+using System.Threading;
 
 class Program
 {
-    public void verify(string arduinoCLIPathInput)
+    static string Verify()
     {
-    if (File.Exists("CLIPath.txt"))
+        string arduinoCLIPathInput = "(Blank Input)";
+        if (File.Exists("CLIPath.txt"))
         {
-        arduinoCLIPathInput = File.ReadAllText("CLIPath.txt");
-        arduinoCLIPathInput = arduinoCLIPathInput.Replace("\"", "");
-        if (!File.Exists(arduinoCLIPathInput))
+            arduinoCLIPathInput = File.ReadAllText("CLIPath.txt");
+            arduinoCLIPathInput = arduinoCLIPathInput.Replace("\"", "");
+            if (!File.Exists(arduinoCLIPathInput))
             {
                 Console.WriteLine("The Arduino CLI executable does not exist at the specified path. Please re-enter.");
-                arduinoCLIPathInput = @Console.ReadLine() ?? string.Empty;
+                arduinoCLIPathInput = Console.ReadLine() ?? string.Empty;
                 File.WriteAllText("CLIPath.txt", arduinoCLIPathInput);
-                }
+            }
         }
         else
         {
@@ -30,16 +31,15 @@ class Program
             {
                 Console.WriteLine("Arduino CLI path accepted.");
             }
-        else
         }
+        return arduinoCLIPathInput;
     }
-    public static void Main(string[] args)
+
+    static void Main(string[] args)
     {
-        string arduinoCliPath = File.ReadAllText("CLIPath.txt");
+        string arduinoCliPath = Verify();
         string sketchDirectory = "..\\..\\..\\..";
         string board = "digistump:avr:digispark-tiny";
-        string currentDirectory = Directory.GetCurrentDirectory();
-
         string[] sketchFiles = Directory.GetFiles(sketchDirectory, "*.ino", SearchOption.AllDirectories);
 
         // Display the list of .ino files to the user
@@ -50,12 +50,13 @@ class Program
         }
         Console.WriteLine("");
         Console.WriteLine("Available settings:");
+
         Console.WriteLine("Change -> Change Arduino CLI Path");
 
         // Prompt the user to select a sketch
         Console.Write("Enter the number of the sketch to upload: ");
         string input = Console.ReadLine() ?? string.Empty;
-        if (input.ToLower() == "change")
+        if (string.Equals(input, "change", StringComparison.OrdinalIgnoreCase))
         {
             Console.Write("Enter the path to your Arduino CLI executable: ");
             string CLIPathInput = Console.ReadLine() ?? string.Empty;
@@ -76,15 +77,13 @@ class Program
         string command = $"{arduinoCliPath} compile -b {board} {sketchPath}";
 
         // Execute the command
-        Process process = new Process();
+        using Process process = new();
         process.StartInfo.FileName = "cmd.exe";
         process.StartInfo.Arguments = $"/c {command}";
         process.StartInfo.RedirectStandardOutput = true;
         process.StartInfo.UseShellExecute = false;
         process.StartInfo.CreateNoWindow = true;
         process.Start();
-
-
 
         // Reads the output and the errors
         string output = process.StandardOutput.ReadToEnd();
@@ -94,23 +93,24 @@ class Program
         process.WaitForExit();
         Console.WriteLine("");
         Console.WriteLine("Sketch Verified");
+        Console.WriteLine("");
+        Console.WriteLine("Plug in Digispark now! (will timeout in 60 seconds)");
         Thread.Sleep(2000);
+
 
         string command2 = $"{arduinoCliPath} upload -b {board} {sketchPath}";
         // Execute the command
-        Process process2 = new Process();
+        using Process process2 = new();
         process2.StartInfo.FileName = "cmd.exe";
         process2.StartInfo.Arguments = $"/c {command2}";
         process2.StartInfo.RedirectStandardOutput = true;
         process2.StartInfo.UseShellExecute = false;
         process2.StartInfo.CreateNoWindow = true;
         process2.Start();
-        string output2 = process.StandardOutput.ReadToEnd();
+        string output2 = process2.StandardOutput.ReadToEnd();
         Console.WriteLine("Output 2:");
-        Console.WriteLine(output);
+        Console.WriteLine(output2);
         process2.WaitForExit();
-
-        
 
         // Check if the upload was successful
         Console.WriteLine("Press enter to close...");
--- picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
+++ picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("picker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+305c42a47ed4ea9e19771fb293843579c20212da")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e23f1a35c68e8c4adfc710fbe194c820704655b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("picker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("picker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
