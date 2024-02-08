7846f48939005132d780b65df8de6656d63d6e4f
huh445 <127354663+huh445@users.noreply.github.com>
2024-02-09 09:57:46 +1100

    I forgor (true)

--- picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
+++ picker/obj/Debug/net8.0/picker.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("picker")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+df7f2291f22e589d7fe84911d81511a4a72926d0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d0bf8107cdf041b101739efd433242fa3f1fe66")]
 [assembly: System.Reflection.AssemblyProductAttribute("picker")]
 [assembly: System.Reflection.AssemblyTitleAttribute("picker")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
