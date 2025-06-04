9372a2c787007a6e953bfce1b35af6300640f5a1
huh445 <charcaf28@outlook.com>
2025-06-04 17:53:48 +1000

    update, fix

--- ChangeBackgroundObf.ps1
+++ ChangeBackgroundObf.ps1
@@ -1,13 +1,21 @@
-$u="aHR0cHM6Ly9kamouZ2VvcmdpYS5nb3Yvc2l0ZXMvZGpqLmdl..."
+$u="aHR0cHM6Ly9tLm1lZGlhLWFtYXpvbi5jb20vaW1hZ2VzL1MvYW16bi1hdXRob3ItbWVkaWEtcHJvZC8ycjYwYzkzY2M4MnZucWVlY3FsMHE3bXE1OC5qcGc="
 $p="$env:USERPROFILE\wp.jpg"
 $wc=New-Object System.Net.WebClient
 $wc.DownloadFile([System.Text.Encoding]::UTF8.GetString([Convert]::FromBase64String($u)),$p)
-$src=@"
+$src = @"
 using System.Runtime.InteropServices;
-public class x{public const int a=20;public const int b=0x01;public const int c=0x02;
-[DllImport("user32.dll",SetLastError=true,CharSet=CharSet.Auto)]
-private static extern int y(int z,int q,string r,int s);
-public static void n(string m){y(a,0,m,b|c);}}
+public class x {
+  public const int SPI_SETDESKWALLPAPER = 20;
+  public const int SPIF_UPDATEINIFILE = 0x01;
+  public const int SPIF_SENDWININICHANGE = 0x02;
+
+  [DllImport("user32.dll", SetLastError = true, CharSet = CharSet.Auto)]
+  private static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+
+  public static void n(string m) {
+    SystemParametersInfo(SPI_SETDESKWALLPAPER, 0, m, SPIF_UPDATEINIFILE | SPIF_SENDWININICHANGE);
+  }
+}
 "@
 Add-Type -TypeDefinition $src
 [x]::n($p)
